57cf3a64135df5af2b3e29437996833da166c788
HayBeeCoder <54000678+HayBeeCoder@users.noreply.github.com>
2022-02-16 23:24:32 +0100

    completed-unit-testing

--- src/Box.js
+++ src/Box.js
@@ -1,4 +1,21 @@
 
+ class Box{
+    constructor(height = 100,width = 100){
+        this.height = height;
+        this.width = width;
+    }
+    area(){
+        return this.height * this.width;
+    }
+    increase(value,dimension){
+        if(dimension == "width"){
+            this.width = this.width + value
+        }
+        if(dimension == "height"){
+            this.height = this.height + value
+        }
+    }
+}
 
+module.exports = Box
 
-// module.exports = Box;
--- test/Box-test.js
+++ test/Box-test.js
@@ -1,3 +1,36 @@
-// const chai = require('chai');
-// const expect = chai.expect;
-// const Box = require('../src/Box');
+const chai = require('chai');
+const expect = chai.expect;
+const Box = require('../src/Box');
+// console.log(Box)
+describe("Box", function(){
+    it("should have height and width default at 100" ,  function(){
+        let box = new Box()
+        expect(box.height).to.equal(100)
+        expect(box.width).to.equal(100)
+    })
+    it("should allow input for height and width" ,  function(){
+        const height = 20
+        const width = 50
+        let box = new Box(height,width)
+        expect(box.height).to.equal(height)
+        expect(box.width).to.equal(width)
+    })
+    // it("should calculate area of the box." ,  function(){
+    //     let box = new Box(87,100)
+    //     expect(box.area()).to.equal(8700)
+        
+    // })
+    // it("should increase width of the box by 90." ,  function(){
+    //     let box = new Box(87,100)
+    //     box.increaseWidth(90, "width")
+    //     expect(box.width).to.equal(190)
+        
+    // })
+    it("should increase height of the box by 900." ,  function(){
+        let box = new Box(87,100)
+        box.increase(900,"height")
+        expect(box.height).to.equal(987)
+        
+    })
+  
+})
